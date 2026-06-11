15743d966fdf3f369a451cb7582cc130889df322
julimare <julimare@f1r7s17.paris.42.school>
2026-06-11 17:57:48 +0200

    merde, la

--- push_swap.h
+++ push_swap.h
@@ -1,67 +1,72 @@
 #ifndef PUSH_SWAP_H
 # define PUSH_SWAP_H
 
-#include <stdio.h>
-#include <unistd.h>
-#include <stdlib.h>
+# include <stdio.h>
+# include <stdlib.h>
+# include <unistd.h>
 
 typedef struct s_node
 {
-    long int valeur;
-    struct s_node *prev;
-    struct s_node *next;
-}   t_node;
+	long int		valeur;
+	int				index;
+	struct s_node	*prev;
+	struct s_node	*next;
+}					t_node;
 
 typedef struct s_stack
 {
-    struct s_node *start;
-    int             size;
-    struct s_node   *end;
-}   t_stack;
+	struct s_node	*start;
+	int				size;
+	struct s_node	*end;
+}					t_stack;
 
 typedef struct data_ve
 {
-    char *strategy;
-    int disorder;
-    int bench;
-}   data_verif;
+	char			*strategy;
+	int				disorder;
+	int				bench;
+}					data_verif;
 
-t_node *ft_new_node(int val);
-void ft_stack_clear(t_stack *stack);
-void init_structs(t_stack *a, t_stack *b, data_verif *verif);
-void ft_data_clear(data_verif *verif);
+t_node				*ft_new_node(int val);
+void				ft_stack_clear(t_stack *stack);
+void				init_structs(t_stack *a, t_stack *b, data_verif *verif);
+void				ft_data_clear(data_verif *verif);
 
-void ft_add_front(t_stack *stack, t_node *node);
-void ft_add_back(t_stack *stack, t_node *node);
-t_node *ft_remove_front(t_stack *stack);
-t_node *ft_remove_last(t_stack *stack);
+void				ft_add_front(t_stack *stack, t_node *node);
+void				ft_add_back(t_stack *stack, t_node *node);
+t_node				*ft_remove_front(t_stack *stack);
+t_node				*ft_remove_last(t_stack *stack);
 
-void ft_ra(t_stack *a);
-void ft_rb(t_stack *b);
-void ft_rr(t_stack *a, t_stack *b);
-void ft_rra(t_stack *a);
-void ft_rrb(t_stack *b);
-void ft_rrr(t_stack *a, t_stack *b);
-void ft_pa(t_stack *a, t_stack *b);
-void ft_pb(t_stack *a, t_stack *b);
-void sa(t_stack *a);
-void sb(t_stack *b);
-void ss(t_stack *a, t_stack *b);
+void				ft_ra(t_stack *a);
+void				ft_rb(t_stack *b);
+void				ft_rr(t_stack *a, t_stack *b);
+void				ft_rra(t_stack *a);
+void				ft_rrb(t_stack *b);
+void				ft_rrr(t_stack *a, t_stack *b);
+void				ft_pa(t_stack *a, t_stack *b);
+void				ft_pb(t_stack *a, t_stack *b);
+void				sa(t_stack *a);
+void				sb(t_stack *b);
+void				ss(t_stack *a, t_stack *b);
 
-long int ft_atoi(char *str);
-int is_digit(char *stack);
-int is_duplicata(char **stack, int argc);
-int	ft_strcmp(const char *s1, const char *s2);
+long int			ft_atoi(char *str);
+int					is_digit(char *stack);
+int					is_duplicata(char **stack, int argc);
+int					ft_strcmp(const char *s1, const char *s2);
 
-void put_error(void);
+void				put_error(void);
 
-float compute_disorder(t_stack *a);
+float				compute_disorder(t_stack *a);
+void				compute_index(t_stack *a, int size);
+void				compute_pos(t_stack *a, int chunk_size, int index);
 
-void algo_simple(t_stack *a, t_stack *b);
+void				algo_simple(t_stack *a, t_stack *b);
+void				algo_medium(t_stack *a, t_stack *b, int size);
 
-void ft_validation(int argc, char **argv, data_verif *verif, t_stack *a);
-void ft_extract_flag(char **argv, data_verif *verif);
+void				ft_validation(int argc, char **argv, data_verif *verif,
+						t_stack *a);
+void				ft_extract_flag(char **argv, data_verif *verif);
 
-int main(int argc, char **argv);
+int					main(int argc, char **argv);
 
 #endif
