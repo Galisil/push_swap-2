8b8520b361ef57f31f2bcfa31979b83b8fb7da73
julimare <julimare@f0r5s11.paris.42.school>
2026-06-23 15:32:24 +0200

    dedcqewd

--- algorithms_medium.c
+++ algorithms_medium.c
@@ -1,14 +1,25 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   algorithms_medium.c                                :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: julimare <julimare@learner.42.tech>        +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2026/06/19 15:18:06 by julimare          #+#    #+#             */
+/*   Updated: 2026/06/19 15:18:11 by julimare         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
 #include "push_swap.h"
 #include <stdio.h>
 
 void	compute_index(t_stack *a, int size)
 {
-	int		min;
-	int		index;
 	t_node	*cursor;
 	t_node	*last_min_find;
+	int		min;
 
-	index = 0;
+	int(index) = 0;
 	while (index < size)
 	{
 		cursor = a->start;
@@ -32,15 +43,16 @@ void	compute_index(t_stack *a, int size)
 	}
 }
 
-void	push_to_b(t_stack *a, t_stack *b, int size, int nb_chunks)
+void	push_to_b(t_stack *a, t_stack *b, int nb_chunks)
 {
 	int	chunk_size;
 	int	min_index;
 	int	max_index;
 	int	nb_pushes;
-	int	j;
+	int	a_size;
 
-	chunk_size = (size / nb_chunks) + 1;
+	a_size = a->size;
+	chunk_size = (a_size / nb_chunks) + 1;
 	min_index = 0;
 	max_index = chunk_size;
 	while (a->start)
@@ -48,98 +60,77 @@ void	push_to_b(t_stack *a, t_stack *b, int size, int nb_chunks)
 		nb_pushes = 0;
 		while (nb_pushes < chunk_size && a->start)
 		{
+			a_size = a->size;
 			if (a->start->index <= max_index)
 			{
 				ft_pb(a, b);
 				nb_pushes++;
 			}
-			else
+			else if (a->start->index <= a_size / 2)
 				ft_ra(a);
-			//voir pour utiliser aussi rb et rra
+			else
+				ft_rra(a);
 		}
 		min_index += chunk_size;
 		max_index += chunk_size;
 	}
 }
 
-// void	algo_simple_cpy(t_stack *a, t_stack *b)
-// {
-//     int     i;
-// 	int		pos_in_b;
-// 	t_node	*cursor;
-
-// 	i = 0;
-// 	while (b->start)
-//     {
-//         cursor = b->start;
-// 		pos_in_b = 0;
-//         while (cursor && cursor->index != i)
-// 		{
-// 			cursor = cursor->next;
-// 			pos_in_b++;
-// 		}
-// 		while (pos_in_b > 0)
-//         {
-//             ft_rb(b);
-//             pos_in_b--;
-//         }printf("b->start->val = %d\n", b->start->valeur);
-// 		ft_pa(a, b);
-// 		printf("a->start->val = %d\n", a->start->valeur);
-// 		i++;
-//     }
-// }
-
-int find_max_pos(t_stack *b)
+int	find_max_pos(t_stack *b)
 {
-    t_node *cur;
-    int max;
-    int pos;
-    int max_pos;
+	t_node	*cur;
+	int		max;
+	int		pos;
+	int		max_pos;
 
-    cur = b->start;
-    max = cur->valeur;
-    pos = 0;
-    max_pos = 0;
-    while (cur)
-    {
-        if (cur->valeur > max)
-        {
-            max = cur->valeur;
-            max_pos = pos;
-        }
-        cur = cur->next;
-        pos++;  
-    }
-    return (max_pos);
+	cur = b->start;
+	max = cur->valeur;
+	pos = 0;
+	max_pos = 0;
+	while (cur)
+	{
+		if (cur->valeur > max)
+		{
+			max = cur->valeur;
+			max_pos = pos;
+		}
+		cur = cur->next;
+		pos++;
+	}
+	return (max_pos);
 }
 
 void	algo_simple_cpy(t_stack *a, t_stack *b)
 {
-    int     max_pos;
+	int	max_pos;
+	int	size_b;
 
 	while (b->start)
-    {
-        max_pos = find_max_pos(b);
-        while (max_pos > 0)
-        {
-            ft_rb(b);
-            max_pos--;
-        }
-        ft_pa(a, b);
-    }
+	{
+		max_pos = find_max_pos(b);
+		while (max_pos > 0)
+		{
+			size_b = b->size;
+			if (max_pos <= size_b / 2)
+				ft_rb(b);
+			else
+				ft_rrb(b);
+			max_pos--;
+		}
+		ft_pa(a, b);
+	}
 }
 
 void	algo_medium(t_stack *a, t_stack *b, int size)
 {
-	int i = 0;
-	t_node *cursor;
+	t_node	*cursor;
 
 	if (size <= 100)
-		push_to_b(a, b, size, 5);
+		push_to_b(a, b, 5);
 	else if (size > 100 && size <= 500)
-		push_to_b(a, b, size, 11);
+		push_to_b(a, b, 11);
 	else
-		push_to_b(a, b, size, 20);
+		push_to_b(a, b, 20);
 	cursor = b->start;
 	if (!cursor)
 	{
